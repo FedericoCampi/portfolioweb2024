cefc7db8a5ac4610cf2fd8547bb8992d6020ba44
Federico Campi <f.campi@aconpy.com>
2024-09-04 20:35:03 -0300

    corrijo import image

--- lib/data.ts
+++ lib/data.ts
@@ -3,7 +3,7 @@ import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import aconpyImg from "@/public/aconpy.png";
-import astrohacksImg from "@/public/astrohacks.png";
+import astrohacksImg from "@/public/astroHacks.png";
 import eycImg from "@/public/e&c.png";
 import mercantilImg from "@/public/mercantil.png";
 
